818b778e910d635a3b070a8e74f11edb2a96f2ae
Todd-Tang <chaotoddtang@gmail.com>
2024-11-14 19:17:24 -0500

    serial

--- motor.py
+++ motor.py
@@ -1,7 +1,9 @@
 import serial
+import time
 #ser = serial.Serial('/dev/serial0', 115200, timeout=1)
 ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
 print(ser.name)
 ser.write(b'$$')
+time.sleep(1)
 #print(ser.readline())
 print(ser.read(10))
