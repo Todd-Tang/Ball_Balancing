c26f9c523338bfe6e2cddf3a3bac9b64bde3b6b8
Todd-Tang <chaotoddtang@gmail.com>
2024-11-14 20:00:24 -0500

    serial

--- motor.py
+++ motor.py
@@ -5,7 +5,7 @@ ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
 print(ser.name)
 time.sleep(1)
 ser.write(b'?\r\n')
-time.sleep(10)
+#time.sleep(1)
 #ser.write(b'$$\r\n')
 #time.sleep(1)
 
@@ -16,8 +16,9 @@ print(ser.readline())
 
 
 
-
+"""""
 message = ser.readline()
 while (b"ok" not in message and b"error" not in message):
     print(message)
     message = ser.readline()
+"""""
