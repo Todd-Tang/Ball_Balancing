d5831e07578155075e4a39670fe35adaab97ec4f
Todd-Tang <chaotoddtang@gmail.com>
2024-11-14 19:38:38 -0500

    serial

--- motor.py
+++ motor.py
@@ -3,6 +3,7 @@ import time
 #ser = serial.Serial('/dev/serial0', 115200, timeout=1)
 ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
 print(ser.name)
+time.sleep(1)
 ser.write(b'$$')
 time.sleep(1)
 #print(ser.readline())
